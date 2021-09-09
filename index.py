5f56e557c9ed524672a3b637ca9b9d9ed6d8b37d
Yash Patil <yash@bit.io>
2021-09-09 17:05:14 -0500

    Initial commit

--- index.py
+++ index.py
@@ -1,12 +1,106 @@
 # coding: utf-8
+from logging import log
 import eel
 import sys
+import bitdotio
+import datetime
+from datetime import timezone
+import uuid
 
+b = bitdotio.bitdotio("YOUR_API_KEY")
+username = "YOUR_USERNAME"
+repo_name = "to_do_view"
 
 @eel.expose
-def hello():
-    print('hello')
+def create_repo():
+    try:
+        r = bitdotio.model.repo.Repo(name=repo_name)
+        r.description = "Our to-do application"
+        r.is_private = True
+        b.create_repo(repo=r)
+    except:
+        pass
 
+    conn = b.get_connection()
+    cur = conn.cursor()
+    cur.execute(f'CREATE TABLE IF NOT EXISTS "{username}/{repo_name}"."lists" \
+    (list_name TEXT, key TEXT, archived BOOLEAN, created DATE, updated DATE)')
+
+    cur.execute(f'CREATE TABLE IF NOT EXISTS "{username}/{repo_name}"."messages" \
+    (message TEXT, key TEXT, id TEXT, completed BOOLEAN, created DATE, updated DATE)')
+
+@eel.expose
+def createNewList(list_name):
+    conn = b.get_connection()
+    cur = conn.cursor()
+    cur.execute(f'INSERT INTO "{username}/{repo_name}"."lists" \
+    VALUES (\'{list_name}\', \'{uuid.uuid4()}\', false, \'{datetime.datetime.now(timezone.utc)}\', \'{datetime.datetime.now(timezone.utc)}\')')
+
+@eel.expose
+def createNewItem(item_name, key):
+    conn = b.get_connection()
+    cur = conn.cursor()
+    cur.execute(f'INSERT INTO "{username}/{repo_name}"."messages" \
+    VALUES (\'{item_name}\', \'{key}\', \'{uuid.uuid4()}\', false, \'{datetime.datetime.now(timezone.utc)}\', \'{datetime.datetime.now(timezone.utc)}\')')
+
+@eel.expose
+def updateItem(id, status):
+    conn = b.get_connection()
+    cur = conn.cursor()
+    cur.execute(f'UPDATE "{username}/{repo_name}"."messages" \
+    SET completed = {status} WHERE id = \'{id}\'')
+
+@eel.expose
+def archiveList(key):
+    conn = b.get_connection()
+    cur = conn.cursor()
+    cur.execute(f'UPDATE "{username}/{repo_name}"."lists" \
+    SET archived = true WHERE key = \'{key}\'')
+
+@eel.expose
+def delete_item(id):
+    conn = b.get_connection()
+    cur = conn.cursor()
+    cur.execute(f'DELETE FROM "{username}/{repo_name}"."messages" \
+    WHERE id = \'{id}\'')
+
+@eel.expose
+def getLists():
+    conn = b.get_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = '{}' AND table_schema = '{}/{}'".format('lists', username, repo_name))
+    columns = cur.fetchall()
+    list_columns_unzipped = [item for t in columns for item in t]
+
+    cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = '{}' AND table_schema = '{}/{}'".format('messages', username, repo_name))
+    columns = cur.fetchall()
+    messages_columns_unzipped = [item for t in columns for item in t]
+
+    cur.execute(f'SELECT * FROM "{username}/{repo_name}"."lists" \
+    WHERE archived = false')
+    data = cur.fetchall()
+
+    all_to_do_lists = []
+
+    for list_item in data:
+        zip_iterator = zip(list_columns_unzipped, list(list_item))
+        to_do_list = dict(zip_iterator)
+
+        to_do_list['list_items'] = []
+
+        key = to_do_list['key']
+        cur.execute(f'SELECT * FROM "{username}/{repo_name}"."messages" WHERE key = \'{key}\'')
+        message_data = cur.fetchall()
+
+        for message in message_data:
+            zip_iterator = zip(messages_columns_unzipped, list(message))
+            message = dict(zip_iterator)
+            to_do_list['list_items'].append(message)
+
+        all_to_do_lists.append(to_do_list)
+
+    return all_to_do_lists
 
 if __name__ == '__main__':
     if sys.argv[1] == '--develop':
@@ -16,7 +110,7 @@ if __name__ == '__main__':
         }, options={
             'port': 8888,
             'host': 'localhost',
-        })
+        }, suppress_error=True)
     else:
         eel.init('build')
         eel.start('index.html')
--- src/App.js
+++ src/App.js
@@ -1,28 +1,102 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect, useRef } from 'react';
+import { Container, Header, Button, Input, Card, Feed, Icon, Loader } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+import './App.css';
 
 import { eel } from "./eel.js";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    eel.set_host("ws://localhost:8888");
-    eel.hello();
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// Point Eel web socket to the instance
+eel.set_host("ws://localhost:8888");
 
-export default App;
+export default function App() {
+  const [newListName, setNewListName] = useState("")
+  const [newItem, setNewItem] = useState("")
+  const [lists, setLists] = useState([])
+  const [loading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    eel.create_repo()
+    refreshLists()
+  }, []);
+
+  const refreshLists = () => {
+    setLoading(true);
+    eel.getLists()(data => {
+      setLists(data)
+      setLoading(false);
+    });
+  }
+
+  const createNewList = () => {
+    eel.createNewList(newListName)(() => {
+      setNewListName("")
+      refreshLists()
+    });
+  }
+
+  const createNewItem = (key) => {
+    eel.createNewItem(newItem, key)(() => {
+      setNewItem("")
+      refreshLists()
+    });
+  }
+
+  const updateTask = (id, status) => {
+    eel.updateItem(id, status)(() => {
+      refreshLists()
+    });
+  }
+
+  const delete_list = (key) => {
+    eel.archiveList(key)(() => {
+      refreshLists()
+    });
+  }
+
+  const delete_item = (id) => {
+    eel.delete_item(id)(() => {
+      refreshLists()
+    });
+  }
+
+  return (
+    <Container style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', paddingTop: '40px'}}>
+      <div>
+        <Header as='h1'>To-Do View</Header>
+        <Loader active={loading} inline='centered' />
+      </div>
+      <br/>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <Input value={newListName} onChange={(e) => setNewListName(e.target.value)} placeholder='List Name' style={{marginRight: '10px'}} />
+        <Button onClick={createNewList}>Create New List</Button>
+      </div>
+      <div style={{paddingTop: '20px'}}>
+        {lists.map((data, index) => (
+          <Card>
+            <Card.Content>
+              <Card.Header>{data.list_name}<Icon onClick={() => delete_list(data.key)} size='small' name='close' style={{color: '#EF4444', marginLeft: '5px'}} /></Card.Header>
+              <div style={{ display: 'flex', flexDirection: 'row', height: '30px', marginTop: '4px' }}>
+                <Input onChange={(e) => setNewItem(e.target.value)} placeholder='Get milk...' style={{marginRight: '10px'}} />
+                <Button onClick={() => createNewItem(data.key)}><Icon size='small' name='add' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}} /></Button>
+              </div>
+            </Card.Content>
+            <Card.Content>
+              <Feed>
+                {data.list_items.map((list_item_data, index) => (
+                  <Feed.Event>
+                    <Feed.Content>
+                      <Feed.Summary onClick={() => updateTask(list_item_data.id, !list_item_data.completed)} style={{ backgroundColor: list_item_data.completed == true ? '#6EE7B7' : '#FDE68A', borderRadius: '5px', padding: '2px', paddingLeft: '10px', color: '#111827' }}>
+                        {list_item_data.message}<Icon onClick={() => delete_item(list_item_data.id)} size='small' name='close' style={{color: '#EF4444', marginLeft: '5px'}} />
+                      </Feed.Summary>
+                    </Feed.Content>
+                  </Feed.Event>  
+                ))}
+              </Feed>
+            </Card.Content>
+          </Card>
+        ))}
+      </div>
+    </Container>
+  );
+}
